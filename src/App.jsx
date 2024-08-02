362565c1125e8dc0fb61a4f679ae10910f886de7
Yuliia <kotyuliya24@gmail.com>
2024-08-01 20:33:53 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -4,43 +4,39 @@ import SearchBox from './components/SearchBox/SearchBox'
 import ContactList from './components/ContactList/ContactList'
 import { useEffect, useState } from 'react'
 
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+]
+  
 function App() {
-  const [
-    contacts = [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    setContacts,
-  ] = useState(() => {
+  const [contacts, setContacts] = useState(() => {
     const savedContacts = window.localStorage.getItem('contact');
-
-    if (savedContacts !== null) {
-      return JSON.parse(savedContacts);
-    }
-
-    return {};
+    return savedContacts ? JSON.parse(savedContacts) : initialContacts;
   });
 
   const [filter, setFilter] = useState('');
 
-  const visibleContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase()),
+  );
 
   useEffect(() => {
     window.localStorage.setItem('contact', JSON.stringify(contacts));
   }, [contacts]);
 
-  const addContact = (newContact) => {
+  const addContact = newContact => {
     setContacts(prevContacts => {
-      return [...prevContacts, newContact]
+      return [...prevContacts, newContact];
     });
   };
 
-  const deleteContact = (contactId) => {
+  const deleteContact = contactId => {
     setContacts(prevContacts => {
       return prevContacts.filter(contact => contact.id !== contactId);
-    })
+    });
   };
 
   return (
